bc54951c6b16fc2d8cdd02507f73857d64299f06
heml12346 <harvyestebanml@gmail.com>
2020-12-25 18:48:08 -0500

    changed order for PORT in index.js

--- server/index.js
+++ server/index.js
@@ -52,4 +52,4 @@ function isAuthenticatedSendToPanel(req, res, next){
 }
 
 
-app.listen(3000 || process.env.PORT, () => console.log("listening on port 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("listening on port 3000"))
